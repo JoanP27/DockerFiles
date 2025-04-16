578ae44338f540426b6b54d2269c64bd694567da
Joan Pomares <joanpomaresherrero@gmail.com>
2025-04-16 17:24:11 +0200

    commit 2

--- Ubuntu-ApacheMySqlServer/init.sql
+++
@@ -1,7 +0,0 @@
--- Establece la contraseña de root
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234';
-
--- Crea usuario phpmyadmin
-CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
